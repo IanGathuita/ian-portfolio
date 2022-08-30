7dab5c15062e960c8cea5a9ebb33f85df55515dc
IanJoseph <Ianjoseph630@gmail.com>
2022-08-30 10:32:03 +0300

    Change favicon and logo, change footer display from flex to grid

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -5,26 +5,21 @@ import './Footer.css';
 const Footer = () => {
     return (
         <footer id="links">
-            
-                
-                <div className="socials">
-                    <h3>Socials</h3>
-                    <ul>
+            <div className="links">
+                <h3>Contact me</h3>
+                <ul>
+                    <li><FaEnvelope></FaEnvelope> <a href="mailto:ianjoseph630@gmail.com">Ianjoseph630@gmail.com</a></li>
+                    <li><FaPhone></FaPhone><a href="tel:+254797211350">+2547 9721 1350</a></li>
+                </ul>
+            </div>
+            <div className="socials">
+                <h3>Socials</h3>
+                <ul>
                     <li><ImTwitter></ImTwitter><a href="http://twitter.com">Twitter</a></li>
                     <li><ImFacebook></ImFacebook><a href="http://facebook.com">Facebook</a></li>
                     <li><RiInstagramFill></RiInstagramFill><a href="http://instagram.com">Instagram</a></li>
-                    </ul>
-                </div>
-                <div className="links">
-                    <h3>Contact me</h3>
-                    <ul>
-                    <li><FaEnvelope></FaEnvelope> <a href="mailto:ianjoseph630@gmail.com">Ianjoseph630@gmail.com</a></li>
-                    <li><FaPhone></FaPhone><a href="tel:+254797211350">+2547 9721 1350</a></li>
-                    </ul>
-                </div>
-
-
-            
+                </ul>
+            </div>
         </footer>
     );
 }
