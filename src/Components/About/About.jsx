3114202702aa5633409dced96cbf7685746a2360
IanGathuita <ianjoseph630@gmail.com>
2026-06-18 12:29:46 +0300

    add container query and other visual improvements

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -4,15 +4,19 @@ import './About.css';
 const About = () => {
     
     return ( 
-        <section className="about" id="about" >
-        <div>
-        <h2>Who am I?</h2>
-        <p>I am a junior software developer who thrives on untangling complex problems with elegant code. 
-            Websites and apps are my tools to <em>bridge the gap between user needs and innovative solutions</em>.
-            I find immense satisfaction in crafting user-centric experiences that have a positive impact.
-        </p>
-        </div>
-        <img  src={IanImg} alt="Ian's passport" className='passport'></img>
+        <section className="about" id="about">
+            <h2>Who am I?</h2>
+            <div className="about__body">
+                               
+                <div className="about__text">
+                    <p>I am a junior software developer who thrives on untangling complex problems with elegant code.
+                        Websites and apps are my tools to <em>bridge the gap between user needs and innovative solutions</em>.
+                        I find immense satisfaction in crafting user-centric experiences that have a positive impact.
+                    </p>
+                </div>
+                <img src={IanImg} alt="Ian's passport" className="passport" /> 
+                
+            </div>
         </section>
      );
 }
